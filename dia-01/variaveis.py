63fdab24f6612cb7d9fe65b8904371e254d0c9f0
Fabiana Amaral <famaral.erp@gmail.com>
2019-07-28 13:59:08 -0300

    tipos e variaveis

---
+++ dia-01/variaveis.py
@@ -0,0 +1,9 @@
+nome = 'Thiago'
+nota = 9.8
+habilitado = True
+print('Nome: ' + nome)
+print('Nota: ' + str(nota))
+print('Habilitado: ' + str(habilitado))
+
+carteira = {'nome':nome, 'nota': nota, 'habilitado': habilitado}
+print(carteira)
